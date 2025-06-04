3eb537ba4b5b507d8b1696954de3d17cc67aa56c
Romeo Ebahim <romeolacerdafarias@gmail.com>
2025-06-04 18:55:20 -0300

    add prettier

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
 import fastify from "fastify";
 import { PlaceOrder } from "./useCases/PlaceOrder";
 
-const app = fastify()
+const app = fastify();
 
-app.post('/orders', async (request, reply) => {
-    const placeOrder = new PlaceOrder()
+app.post("/orders", async (request, reply) => {
+    const placeOrder = new PlaceOrder();
 
-    const { orderId } = await placeOrder.execute()
+    const { orderId } = await placeOrder.execute();
 
-    reply.status(201).send({ orderId })
-})
+    reply.status(201).send({ orderId });
+});
 
-
-app.listen({ port: 3000 })
-    .then(() => {
-        console.log('🔥 Server running at http://localhost:3000')
-    })
+app.listen({ port: 3000 }).then(() => {
+    console.log("🔥 Server running at http://localhost:3000");
+});
