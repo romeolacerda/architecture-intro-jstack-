370846f1200bdfc9efd6055c7c106d1c03a75e1c
Romeo Ebahim <romeolacerdafarias@gmail.com>
2025-06-07 19:10:28 -0300

    registry singleton

--- src/di/Registry.ts
+++ src/di/Registry.ts
@@ -2,7 +2,19 @@
 type Constructor<T> = new (...args: any[]) => T
 
 export class Registry {
+    private static instance: Registry
+    static getInstance(){
+        if(!this.instance){
+            this.instance = new Registry()
+        }
+
+        return this.instance
+    }
+
     private readonly services: Map<string, Constructor<any>> = new Map()
+
+    private constructor(){}
+
 
     register<T>(implementation: Constructor<T>) {
         const token = implementation.name
